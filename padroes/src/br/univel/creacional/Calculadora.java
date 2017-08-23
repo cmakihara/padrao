3b42f208c034fd41807b5ccd63247573f0a73651
cmakihara <clmakihara>
2017-08-22 22:19:01 -0300

    3

--- padroes/src/br/univel/creacional/Calculadora.java
+++ padroes/src/br/univel/creacional/Calculadora.java
@@ -5,23 +5,28 @@ public class Calculadora {
 	private float resultado =0;
 	
 	public Calculadora mais(int num){
+		System.out.println("+"+num);
 		this.resultado+=num;
 		return this;
 	}
 	public Calculadora menos(int num){
+		System.out.println("-"+num);
 		this.resultado-=num;
 		return this;	
 	}
 	public Calculadora dividePor(int num){
+		System.out.println("/"+num);
 		this.resultado=this.resultado/num;
 		return this;
 	}
 	public Calculadora multiplicaPor(int num){
+		System.out.println("*"+num);
 		this.resultado=this.resultado*num;
 		return this;
 	}
-	public Calculadora limpar(){
+	public Calculadora limpar(){		
 		this.resultado = 0;
+		System.out.println("C"+this.resultado);
 		return this;
 	}
 	public Calculadora resultado(){
